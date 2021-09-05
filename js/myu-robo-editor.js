fe9e7b76ed2858b8ac1378bca046110e93390cc1
shogenji <ruirui@ruirui.jp>
2021-09-05 12:49:15 +0900

    small fix

--- js/myu-robo-editor.js
+++ js/myu-robo-editor.js
@@ -28,8 +28,8 @@ function startup() {
     objLoadProgram.addEventListener('change', loadProgram);
 
     objSelectCommand.addEventListener('change', setDescription, false);
-    // objSelectCommand.addEventListener('dblclick', addCommand, false);
-
+    objProgramTA.addEventListener('keydown', onKeydown, false);
+    
     setVersion();
     setSelectBox();
 
@@ -88,6 +88,21 @@ function setSelectBox() {
 
 function setDescription() {
     objCommandDescription.innerText = commandDictionary[objSelectCommand.value][2];
+}
+
+function onKeydown(event) {
+    if (event.keyCode != 9) {
+      return;
+    }
+
+    event.preventDefault();
+
+    let carretPosition = objProgramTA.selectionStart;
+    let carretLeft     = objProgramTA.value.substr(0, carretPosition);
+    let carretRight    = objProgramTA.value.substr(carretPosition, objProgramTA.value.length);
+
+    objProgramTA.value = carretLeft + "\t" + carretRight;
+    objProgramTA.selectionEnd = carretPosition+1;
 }
 
 
